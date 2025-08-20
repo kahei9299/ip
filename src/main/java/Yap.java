1be2788a820a3c7b50e0ebd7b1ad746c3ff920b8
kahei9299 <liangkahei03@gmail.com>
2025-08-20 12:07:18 +0800

    Added greetings, and exit

--- src/main/java/Yap.java
+++ src/main/java/Yap.java
@@ -1,10 +1,13 @@
+import java.util.Scanner;
+
 public class Yap {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello! I'm Yap your new best friend!");
+        System.out.println("May I know what's your name?\n");
+        String name = sc.nextLine();
+        System.out.printf("Hi %s, how may i help you today? :D\n", name);
+        System.out.println("Alright it was nice talking to you %s! See you next time :D");
     }
 }
