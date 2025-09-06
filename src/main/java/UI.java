e62e213f2ec94d403835e4e01bf8d8a145298674
kahei9299 <liangkahei03@gmail.com>
2025-09-06 11:22:16 +0800

    completed oop extractions and integration

--- src/main/java/UI.java
+++ src/main/java/UI.java
@@ -1,10 +1,22 @@
 import java.util.Scanner;
 
-public class UI {
+public class Ui {
     private final Scanner in = new Scanner(System.in);
 
     public void showWelcome() {
-        System.out.println("Hello! I'm Yap\nWhat can I do for you?");
+        System.out.println("Hello! I'm Yap your new best friend!");
+    }
+
+    public String askName() {
+        System.out.println("May I know what's your name?");
+        String name = in.nextLine().trim();
+        while (name.isEmpty()) {
+            System.out.println("Sorry, I didn't catch that. What's your name?");
+            name = in.nextLine().trim();
+        }
+        System.out.printf("Nice to meet you, %s!%n", name);
+        System.out.printf("How can i help you today?");
+        return name;
     }
 
     public String readCommand() {
@@ -27,7 +39,7 @@ public class UI {
         showError("I couldn't load your saved tasks. Starting fresh!");
     }
 
-    public void showGoodbye() {
-        System.out.println("Bye. Hope to see you again soon!");
+    public void showGoodbye(String name) {
+        System.out.printf("Goodbye, %s!%n", name);
     }
 }
