048827690eb73eb38de88182861839feeeba43f4
kahei9299 <liangkahei03@gmail.com>
2025-09-07 17:06:37 +0800

    added java docs

--- src/main/java/yap/task/TaskList.java
+++ src/main/java/yap/task/TaskList.java
@@ -3,6 +3,14 @@ package yap.task;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Maintains an ordered list of {@link Task} objects and provides
+ * operations to add, remove, access, and render tasks for display.
+ * <p>
+ * Responsibilities: storage, 1-based access semantics, string rendering.
+ * Collaborators: {@link Task} and its subclasses.
+ */
+
 public class TaskList {
     private final List<Task> tasks;
 
@@ -14,16 +22,40 @@ public class TaskList {
         this.tasks = new ArrayList<>(initial);
     }
 
+    /**
+     * Returns the number of tasks currently in the list.
+     *
+     * @return number of tasks
+     */
     public int size() { return tasks.size(); }
 
+    /**
+     * Returns the task at the given 1-based index.
+     *
+     * @param index1Based the index starting at 1
+     * @return the task at the specified index
+     * @throws IndexOutOfBoundsException if the index is invalid
+     */
     public Task get(int index1Based) {
         return tasks.get(index1Based - 1);
     }
 
+    /**
+     * Adds a new task to the list.
+     *
+     * @param t the task to add
+     */
     public void add(Task t) {
         tasks.add(t);
     }
 
+    /**
+     * Removes the task at the given 1-based index.
+     *
+     * @param index1Based the index of the task to remove
+     * @return the removed task
+     * @throws IndexOutOfBoundsException if the index is invalid
+     */
     public Task remove(int index1Based) {
         return tasks.remove(index1Based - 1);
     }
@@ -32,6 +64,11 @@ public class TaskList {
         return new ArrayList<>(tasks);
     }
 
+    /**
+     * Returns a string rendering of all tasks for display.
+     *
+     * @return a multi-line string of tasks
+     */
     public String render() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < tasks.size(); i++) {
